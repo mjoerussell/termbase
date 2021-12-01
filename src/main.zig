66b1ddcf1265bc4ae59896e4febd8cf0ef3f70aa
Mitch <mjoerussell@gmail.com>
2021-11-30 22:02:24 -0600

    Big refactor, bugfixes, adding features:

--- src/main.zig
+++ src/main.zig
@@ -3,200 +3,15 @@ const Allocator = std.mem.Allocator;
 
 const sdl = @import("sdl2");
 const sdl_ttf = @import("sdl_ttf.zig");
-const Font = sdl_ttf.Font;
 
 const zdb = @import("zdb");
 
+const TextArea = @import("text_area.zig").TextArea;
+
 // @todo - Will be modified as things are done/new tasks are required
-// * Move cursor with arrow keys
-// * Figure out why running 2 queries results in FunctionSequenceError
 // * Set up arbitrary db connections
 // * Multiple modes - text/command/query
-
-const OdbcTestType = struct {
-    id: u32,
-    name: []const u8,
-    occupation: []const u8,
-    age: u32,
-
-    fn deinit(self: *OdbcTestType, allocator: *Allocator) void {
-        allocator.free(self.name);
-        allocator.free(self.occupation);
-    }
-};
-
-const TextArea = struct {
-    // Custom writer that handles writing at the current cursor position and moving the cursor
-    // without having to handle that outside of the TextArea context
-    const Writer = std.io.Writer(*TextArea, error{OutOfMemory}, writeAtCursor);
-
-    buffer: std.ArrayList(u8),
-    rect: sdl.Rectangle,
-
-    cursor: sdl.Rectangle,
-    cursor_pos: usize,
-
-    fn init(allocator: *Allocator, x: c_int, y: c_int) TextArea {
-        return TextArea{
-            .buffer = std.ArrayList(u8).init(allocator),
-            .rect = sdl.Rectangle{
-                .x = x,
-                .y = y,
-                .width = 0,
-                .height = 0,
-            },
-            .cursor = sdl.Rectangle{
-                .x = 0,
-                .y = 0,
-                // @note This is hardcoded based on the debug font size - should change in the future
-                .width = 7,
-                .height = 15,
-            },
-            .cursor_pos = 0,
-        };
-    }
-
-    fn deinit(text_area: *TextArea) void {
-        text_area.buffer.deinit();
-    }
-
-    fn render(text_area: *TextArea, renderer: sdl.Renderer, font: Font, active: bool) !void {
-        try renderer.setColorRGBA(255, 255, 255, 255);
-
-        try renderer.fillRect(text_area.rect);
-
-        const inner_rect = sdl.Rectangle{
-            .x = text_area.rect.x + 2,
-            .y = text_area.rect.y + 2,
-            .width = text_area.rect.width - 4,
-            .height = text_area.rect.height - 4,
-        };
-
-        try renderer.setColorRGBA(0, 0, 0, 0);
-        try renderer.fillRect(inner_rect);
-
-        try renderer.setColorRGBA(255, 255, 255, 255);
-
-        const text_dim = try font.drawText(renderer, text_area.buffer.items, text_area.rect.x + 1, text_area.rect.y + 1);
-        text_area.rect.width = text_dim.x - text_area.rect.x + 2;
-        text_area.rect.height = text_dim.y - text_area.rect.y + 2;
-
-        if (active) {
-            const text_to_cursor = text_area.buffer.items[0..text_area.cursor_pos];
-            const text_to_cursor_size = font.sizeText(text_to_cursor);
-
-            var newline_count_to_cursor: c_int = 0;
-            var last_newline_index: usize = 0;
-            for (text_to_cursor) |char, char_index| {
-                if (char == '\n') {
-                    last_newline_index = char_index;
-                    newline_count_to_cursor += 1;
-                }
-            }
-
-            const size_start_index = if (text_area.buffer.items.len > 0 and last_newline_index < text_area.buffer.items.len - 1)
-                last_newline_index + 1
-            else
-                last_newline_index;
-
-            const text_newline_to_cursor_size = font.sizeText(text_to_cursor[size_start_index..]);
-            const cursor_x = text_area.rect.x + text_newline_to_cursor_size.width;
-            const cursor_y = text_area.rect.y + (text_to_cursor_size.height * newline_count_to_cursor);
-
-            try renderer.setColorRGBA(255, 255, 255, 255);
-            try renderer.fillRect(sdl.Rectangle{
-                .x = cursor_x,
-                .y = cursor_y,
-                .width = text_area.cursor.width,
-                .height = text_area.cursor.height,
-            });
-        }
-    }
-
-    fn isInside(text_area: TextArea, x: c_int, y: c_int) bool {
-        return x >= text_area.rect.x and x <= text_area.rect.x + text_area.rect.width and y >= text_area.rect.y and y <= text_area.rect.y + text_area.rect.height;
-    }
-
-    fn writeAtCursor(text_area: *TextArea, bytes: []const u8) !usize {
-        try text_area.buffer.insertSlice(text_area.cursor_pos, bytes);
-        text_area.cursor_pos += bytes.len;
-        return bytes.len;
-    }
-
-    fn writer(text_area: *TextArea) Writer {
-        return .{ .context = text_area };
-    }
-
-    fn moveCursorRight(text_area: *TextArea, span: usize) void {
-        if (text_area.buffer.items.len == 0) return;
-        text_area.cursor_pos += span;
-        if (text_area.cursor_pos >= text_area.buffer.items.len) {
-            text_area.cursor_pos = text_area.buffer.items.len - 1;
-        }
-    }
-
-    fn moveCursorLeft(text_area: *TextArea, span: usize) void {
-        if (span > text_area.cursor_pos) {
-            text_area.cursor_pos = 0;
-        } else {
-            text_area.cursor_pos -= span;
-        }
-    }
-
-    fn moveCursorUp(text_area: *TextArea, span: usize) void {
-        var char_index: usize = text_area.cursor_pos;
-        if (char_index == text_area.buffer.items.len) {
-            char_index -= 1;
-        }
-
-        var newlines_seen: usize = 0;
-        var current_line_column: usize = 0;
-        while (char_index > 0) : (char_index -= 1) {
-            if (text_area.buffer.items[char_index] == '\n') {
-                if (newlines_seen == 0) {
-                    current_line_column = text_area.cursor_pos - char_index;
-                }
-                newlines_seen += 1;
-                if (newlines_seen == span + 1) {
-                    text_area.cursor_pos = char_index + current_line_column;
-                    return;
-                }
-            }
-        }
-
-        text_area.cursor_pos = current_line_column;
-    }
-
-    fn moveCursorDown(text_area: *TextArea, span: usize) void {
-        const current_line_column: usize = blk: {
-            var nearest_newline_index: usize = text_area.cursor_pos;
-            while (nearest_newline_index >= 0) : (nearest_newline_index -= 1) {
-                if (text_area.buffer.items[nearest_newline_index] == '\n') {
-                    break :blk text_area.cursor_pos - nearest_newline_index;
-                }
-            }
-
-            break :blk text_area.cursor_pos;
-        };
-
-        var newlines_seen: usize = 0;
-        var char_index: usize = text_area.cursor_pos;
-        while (char_index < text_area.buffer.items.len) : (char_index += 1) {
-            if (text_area.buffer.items[char_index] == '\n') {
-                newlines_seen += 1;
-                if (newlines_seen == span) {
-                    text_area.cursor_pos = char_index + current_line_column;
-                    if (text_area.cursor_pos >= text_area.buffer.items.len) {
-                        text_area.cursor_pos = text_area.buffer.items.len - 1;
-                    }
-                    return;
-                }
-            }
-        }
-
-        text_area.cursor_pos = text_area.buffer.items.len - 1;
-    }
-};
+// * Properly space columns
 
 pub fn main() anyerror!void {
     try sdl.init(.{ .video = true, .events = true });
@@ -216,9 +31,8 @@ pub fn main() anyerror!void {
     try connection.setCommitMode(.auto);
 
     var cursor = try connection.getCursor(allocator);
-    defer cursor.deinit() catch {};
 
-    var window = try sdl.createWindow("Termbase", .{ .centered = {} }, .{ .centered = {} }, 640, 480, .{ .shown = true });
+    var window = try sdl.createWindow("Termbase", .{ .centered = {} }, .{ .centered = {} }, 720, 480, .{ .shown = true, .resizable = true });
     defer window.destroy();
 
     var renderer = try sdl.createRenderer(window, null, .{ .accelerated = true });
@@ -283,31 +97,104 @@ pub fn main() anyerror!void {
                         switch (key_ev.keycode) {
                             .@"return" => {
                                 if (key_ev.modifiers.get(.left_control) or key_ev.modifiers.get(.right_control)) {
-                                    // @todo Some modification to executeDirect that doesn't need to know the result type when
-                                    // running the query. Without that, it's kind of impossible to run arbitrary queries
-                                    var result_set = try cursor.executeDirect(OdbcTestType, .{}, ta.buffer.items);
-                                    defer result_set.deinit();
+                                    defer cursor.close() catch {};
 
-                                    try text_areas.append(TextArea.init(allocator, ta.rect.x, ta.rect.y + ta.rect.height));
-                                    active_text_area = &text_areas.items[text_areas.items.len - 1];
+                                    if (ta.child) |result_ta| {
+                                        active_text_area = result_ta;
+                                        active_text_area.?.clear();
+                                    } else {
+                                        var child_ta = ta.spawnChild(allocator);
+                                        try text_areas.append(child_ta);
+                                        active_text_area = &text_areas.items[text_areas.items.len - 1];
+                                        ta.child = active_text_area;
+                                    }
 
                                     var result_writer = active_text_area.?.writer();
-                                    result_writer.writeAll("id | name | occupation | age\n") catch {};
-                                    while (try result_set.next()) |*item| {
-                                        result_writer.print("{} | {s} | {s} | {}\n", .{ item.id, item.name, item.occupation, item.age }) catch {};
-                                        item.deinit(allocator);
+
+                                    var result_set = cursor.executeDirect(ta.buffer.items, .{}) catch {
+                                        const diag_recs = cursor.getErrors();
+                                        defer cursor.allocator.free(diag_recs);
+
+                                        for (diag_recs) |*rec, rec_index| {
+                                            result_writer.writeAll(rec.error_message) catch {};
+                                            if (rec_index < diag_recs.len - 1) {
+                                                result_writer.writeAll("\n") catch {};
+                                            }
+                                            rec.deinit(cursor.allocator);
+                                        }
+                                        continue;
+                                    };
+
+                                    var row_iter = try result_set.rowIterator();
+                                    defer row_iter.deinit();
+
+                                    var first_row = true;
+                                    while (true) {
+                                        var next_row = row_iter.next() catch continue;
+                                        var row = next_row orelse break;
+
+                                        if (first_row) {
+                                            // First time around, print the column headings
+                                            for (row.columns) |column, column_index| {
+                                                result_writer.print("{s}", .{column.name}) catch {};
+                                                if (column_index == row.columns.len - 1) {
+                                                    result_writer.writeAll("\n") catch {};
+                                                } else {
+                                                    result_writer.writeAll(" | ") catch {};
+                                                }
+                                            }
+                                            first_row = false;
+                                        }
+
+                                        var column_index: usize = 1;
+                                        while (column_index <= row.columns.len) : (column_index += 1) {
+                                            row.printColumnAtIndex(column_index, .{}, result_writer) catch {};
+                                            if (column_index == row.columns.len) {
+                                                result_writer.writeAll("\n") catch {};
+                                            } else {
+                                                result_writer.writeAll(" | ") catch {};
+                                            }
+                                        }
                                     }
                                 } else {
                                     writer.writeAll("\n") catch {};
                                 }
                             },
                             .tab => {
-                                writer.writeAll("  ") catch {};
+                                if (key_ev.modifiers.get(.left_control) or key_ev.modifiers.get(.right_control)) {
+                                    if (key_ev.modifiers.get(.left_shift) or key_ev.modifiers.get(.right_shift)) {
+                                        for (text_areas.items) |*other_ta| {
+                                            if (other_ta.child != null and other_ta.child.? == ta) {
+                                                active_text_area = other_ta;
+                                            }
+                                        }
+                                    } else {
+                                        if (ta.child) |child| {
+                                            active_text_area = child;
+                                        }
+                                    }
+                                } else {
+                                    writer.writeAll("  ") catch {};
+                                }
+                            },
+                            .d => {
+                                if (key_ev.modifiers.get(.left_control) or key_ev.modifiers.get(.right_control)) {
+                                    var ta_dup = TextArea.init(allocator, ta.rect.x + ta.rect.width + 10, ta.rect.y);
+                                    ta_dup.writer().writeAll(ta.buffer.items) catch {};
+
+                                    try text_areas.append(ta_dup);
+                                    active_text_area = &text_areas.items[text_areas.items.len - 1];
+                                }
                             },
                             .backspace => {
                                 if (ta.buffer.items.len >= 1) {
-                                    ta.cursor_pos -= 1;
-                                    _ = ta.buffer.pop();
+                                    if (ta.cursor_pos == ta.buffer.items.len) {
+                                        _ = ta.buffer.pop();
+                                        ta.cursor_pos -= 1;
+                                    } else if (ta.cursor_pos != 0) {
+                                        _ = ta.buffer.orderedRemove(ta.cursor_pos - 1);
+                                        ta.cursor_pos -= 1;
+                                    }
                                 }
                             },
                             .left => ta.moveCursorLeft(1),
---
+++ src/text_area.zig
@@ -0,0 +1,198 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+
+const sdl = @import("sdl2");
+
+const sdl_ttf = @import("sdl_ttf.zig");
+const Font = sdl_ttf.Font;
+
+pub const TextArea = struct {
+    // Custom writer that handles writing at the current cursor position and moving the cursor
+    // without having to handle that outside of the TextArea context
+    pub const Writer = std.io.Writer(*TextArea, error{OutOfMemory}, writeAtCursor);
+
+    buffer: std.ArrayList(u8),
+    rect: sdl.Rectangle,
+
+    cursor: sdl.Rectangle,
+    cursor_pos: usize,
+
+    child: ?*TextArea = null,
+
+    pub fn init(allocator: *Allocator, x: c_int, y: c_int) TextArea {
+        return TextArea{
+            .buffer = std.ArrayList(u8).init(allocator),
+            .rect = sdl.Rectangle{
+                .x = x,
+                .y = y,
+                .width = 0,
+                .height = 0,
+            },
+            .cursor = sdl.Rectangle{
+                .x = 0,
+                .y = 0,
+                // @note This is hardcoded based on the debug font size - should change in the future
+                .width = 7,
+                .height = 15,
+            },
+            .cursor_pos = 0,
+        };
+    }
+
+    pub fn deinit(text_area: *TextArea) void {
+        text_area.buffer.deinit();
+    }
+
+    pub fn spawnChild(text_area: *TextArea, allocator: *Allocator) TextArea {
+        return TextArea.init(allocator, text_area.rect.x, text_area.rect.y + text_area.rect.height);
+    }
+
+    pub fn render(text_area: *TextArea, renderer: sdl.Renderer, font: Font, active: bool) !void {
+        try renderer.setColorRGBA(255, 255, 255, 255);
+
+        try renderer.fillRect(text_area.rect);
+
+        const inner_rect = sdl.Rectangle{
+            .x = text_area.rect.x + 2,
+            .y = text_area.rect.y + 2,
+            .width = text_area.rect.width - 4,
+            .height = text_area.rect.height - 4,
+        };
+
+        try renderer.setColorRGBA(0, 0, 0, 0);
+        try renderer.fillRect(inner_rect);
+
+        try renderer.setColorRGBA(255, 255, 255, 255);
+
+        const text_dim = try font.drawText(renderer, text_area.buffer.items, text_area.rect.x + 2, text_area.rect.y + 2);
+        text_area.rect.width = text_dim.x - text_area.rect.x + 2;
+        text_area.rect.height = text_dim.y - text_area.rect.y + 2;
+
+        if (active) {
+            // If this text area is the current active text area, draw the cursor at cursor_pos in the text
+            const text_to_cursor = text_area.buffer.items[0..text_area.cursor_pos];
+            const text_to_cursor_size = font.sizeText(text_to_cursor);
+
+            var newline_count_to_cursor: c_int = 0;
+            var last_newline_index: usize = 0;
+            for (text_to_cursor) |char, char_index| {
+                if (char == '\n') {
+                    last_newline_index = char_index;
+                    newline_count_to_cursor += 1;
+                }
+            }
+
+            const size_start_index = if (text_area.buffer.items.len > 0 and last_newline_index < text_area.buffer.items.len - 1)
+                last_newline_index + 1
+            else
+                last_newline_index;
+
+            const text_newline_to_cursor_size = font.sizeText(text_to_cursor[size_start_index..]);
+            const cursor_x = text_area.rect.x + text_newline_to_cursor_size.width + text_area.cursor.width;
+            const cursor_y = text_area.rect.y + (text_to_cursor_size.height * newline_count_to_cursor) + 2;
+
+            try renderer.setColorRGBA(255, 255, 255, 255);
+            try renderer.fillRect(sdl.Rectangle{
+                .x = cursor_x,
+                .y = cursor_y,
+                .width = text_area.cursor.width,
+                .height = text_area.cursor.height,
+            });
+        }
+    }
+
+    pub fn isInside(text_area: TextArea, x: c_int, y: c_int) bool {
+        return x >= text_area.rect.x and x <= text_area.rect.x + text_area.rect.width and y >= text_area.rect.y and y <= text_area.rect.y + text_area.rect.height;
+    }
+
+    pub fn writeAtCursor(text_area: *TextArea, bytes: []const u8) !usize {
+        try text_area.buffer.insertSlice(text_area.cursor_pos, bytes);
+        text_area.cursor_pos += bytes.len;
+        return bytes.len;
+    }
+
+    pub fn writer(text_area: *TextArea) Writer {
+        return .{ .context = text_area };
+    }
+
+    pub fn clear(text_area: *TextArea) void {
+        text_area.buffer.items.len = 0;
+        text_area.cursor_pos = 0;
+    }
+
+    pub fn moveCursorRight(text_area: *TextArea, span: usize) void {
+        if (text_area.buffer.items.len == 0) return;
+        text_area.cursor_pos += span;
+        if (text_area.cursor_pos > text_area.buffer.items.len) {
+            text_area.cursor_pos = text_area.buffer.items.len;
+        }
+    }
+
+    pub fn moveCursorLeft(text_area: *TextArea, span: usize) void {
+        if (span > text_area.cursor_pos) {
+            text_area.cursor_pos = 0;
+        } else {
+            text_area.cursor_pos -= span;
+        }
+    }
+
+    pub fn moveCursorUp(text_area: *TextArea, span: usize) void {
+        var char_index: usize = text_area.cursor_pos;
+        if (char_index == text_area.buffer.items.len) {
+            char_index -= 1;
+        }
+
+        var newlines_seen: usize = 0;
+        var current_line_column: usize = 0;
+        while (char_index > 0) : (char_index -= 1) {
+            if (text_area.buffer.items[char_index] == '\n') {
+                if (newlines_seen == 0) {
+                    current_line_column = text_area.cursor_pos - char_index;
+                }
+                newlines_seen += 1;
+                if (newlines_seen == span + 1) {
+                    text_area.cursor_pos = char_index + current_line_column;
+                    return;
+                }
+            }
+        }
+
+        text_area.cursor_pos = current_line_column;
+    }
+
+    pub fn moveCursorDown(text_area: *TextArea, span: usize) void {
+        const current_line_column: usize = blk: {
+            var nearest_newline_index: usize = text_area.cursor_pos;
+            while (nearest_newline_index >= 0) {
+                if (text_area.buffer.items[nearest_newline_index] == '\n') {
+                    break :blk text_area.cursor_pos - nearest_newline_index;
+                }
+
+                if (nearest_newline_index > 0) {
+                    nearest_newline_index -= 1;
+                } else {
+                    break;
+                }
+            }
+
+            break :blk text_area.cursor_pos;
+        };
+
+        var newlines_seen: usize = 0;
+        var char_index: usize = text_area.cursor_pos;
+        while (char_index < text_area.buffer.items.len) : (char_index += 1) {
+            if (text_area.buffer.items[char_index] == '\n') {
+                newlines_seen += 1;
+                if (newlines_seen == span) {
+                    text_area.cursor_pos = char_index + current_line_column;
+                    if (text_area.cursor_pos >= text_area.buffer.items.len) {
+                        text_area.cursor_pos = text_area.buffer.items.len - 1;
+                    }
+                    return;
+                }
+            }
+        }
+
+        text_area.cursor_pos = text_area.buffer.items.len - 1;
+    }
+};
